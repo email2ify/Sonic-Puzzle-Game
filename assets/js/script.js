217fa4d9118d6409d4fc490ca1ecb942c05e8670
email2ify <email2ify@yahoo.com>
2022-11-01 10:12:23 +0000

    Add basic structure for Html/Css

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+// the 5 by 5 stable view
+let ftable = 5;
+let stable = 5;
+
+let perform = 0;
+
+window.onload = function () {
+    for (let f = 0; f < ftable; f++) {
+        for (let s = 0; s < stable; s++) {
+
+            let floor = document.createElement("img");
+            floor.src = "./images.jpg";
+
+            document.getElementById("table").append(floor);
+        }
+    }
+    // From 1 to 25pcs of the image puzzle using array
+    
+    let parts = [];
+    for (let i = 1; i <= ftable * stable; i++) {
+        parts.push(i.toString());
+
+    }
+    for (let i = 0; i < parts.length; i++) {
+        let floor = document.createElement("img");
+        floor.src = "./images/" + parts[i] + ".jpg";
+
+        document.getElementById("parts").append(floor);
+    }
+}
