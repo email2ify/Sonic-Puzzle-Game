304d6921b66d16c1a87e47bb99c62bcfa03582e9
email2ify <email2ify@yahoo.com>
2022-11-08 23:34:54 +0000

    Add readme content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,21 @@
 
-
 // first and second table
 let ftable = 5;
 let stable = 5;
 // for the 5 by 5 swapping and dragging tables
 let firstFloorr;
 let secondFloorr;
-// it keeps information of drag performers, starting from zero after  refresh
+// it keeps information of drag performers, starting from zero after refresh
 let perform = 0;
-// initializing tables here 
-   
+
+
+let order = ["puzzle1", "puzzle2", "puzzle3", "puzzle4", "puzzle5", "puzzle6", "puzzle7", "puzzle8", "puzzle9", "puzzle10", "puzzle11", "puzzle12", "puzzle13", "puzzle14", "puzzle15", "puzzle16", "puzzle17", "puzzle18", "puzzle19", "puzzle20", "puzzle21", "puzzle22", "puzzle23", "puzzle24", "puzzle25"];
+// setting tables here 
 window.onload = function () {
     for (let f = 0; f < ftable; f++) {
         for (let s = 0; s < stable; s++) {
-            let order = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25'];
-            //<img id="0" src="puzzle1.jpg">
             let floorr = document.createElement("img");
-            floorr.id = f.toString() + "-" + s.toString();
-           // floorr.src = order.shift() + ".jpg";
-            
+
             // hereis the key event listener for starting,draging over,enter & leave,dragdrop and finish for the first Table
             floorr.addEventListener("dragstart", dragStart);
             floorr.addEventListener("dragover", dragOver);
@@ -28,26 +25,29 @@ window.onload = function () {
             floorr.addEventListener("dragend", dragEnd);
 
             document.getElementById("table").append(floorr);
+
         }
     }
-    // all images from 1 to 25 in the parts section using array 
-    
+    // all images from "puzzle1" to "puzzle25" in the parts section using array 
     let parts = [];
     for (let i = 1; i <= ftable * stable; i++) {
         parts.push(i.toString());
-       
     }
-    // add a Math.ramdom of the array so each time a refresh is made, the images changes positions 
+    // add a Math.ramdom of the array so each time a refresh is made, the images changes positions
+    parts.reverse();
     for (let i = 0; i < parts.length; i++) {
         let m = Math.floor(Math.random() * parts.length);
+        //swapping parts
         let cap = parts[i];
         parts[i] = parts[m];
         parts[m] = cap;
 
     }
+
     for (let i = 0; i < parts.length; i++) {
         let floorr = document.createElement("img");
         floorr.src = "assets/images/puzzle" + parts[i] + ".jpg";
+
         //  the key event listener for starting,draging over,enter & leave,dragdrop and finish for the second Table
         floorr.addEventListener("dragstart", dragStart);
         floorr.addEventListener("dragover", dragOver);
@@ -56,12 +56,14 @@ window.onload = function () {
         floorr.addEventListener("drop", dragDrop);
         floorr.addEventListener("dragend", dragEnd);
         document.getElementById("parts").append(floorr);
-
     }
 }
 //  the drag functionality when starting,draging over,enter & leave,dragdrop and finish
+
 function dragStart() {
+    //this is for img floorr being dragged
     firstFloorr = this;
+
 }
 
 function dragOver(i) {
@@ -76,6 +78,7 @@ function dragLeave() {
 
 }
 function dragDrop() {
+    //this is for the img floorr being dropped on
     secondFloorr = this;
 }
 
@@ -90,15 +93,17 @@ function dragEnd() {
     secondFloorr.src = firstImg;
     perform += 1;
     document.getElementById("perform").innerText = perform;
+
+    checkIfSolved()
 }
-
-
-function checkIfSolved(){
-    let userAnswer = document.querySelectorAll("img");
-    let correct = correctOrder()
-    let isCorrect = userAnswer == correct["img"];
-    if (isCorrect) {
+function checkIfSolved() {
+    let userAnswer = document.querySelectorAll(".table");
+    let correct = document.getElementById(".table")
+    if (userAnswer === correct) {
+        correct = true;
+    }
+    if (correct) {
         alert("Hey! CONGRATULATIONS! :D");
-       
+
     }
 }
